176c211d52c607b5fe77b3c0b727a62d24e91b8e
Студент <>
2019-09-05 11:30:14 +0500

    init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashset = new HashSet<long>(); 
+
+            for (var i = 2; i <= 100; i++)
+                for (var j = 2; j <= 100; j++)
+                {
+                    var number = (long)Math.Pow(i, j);
+                    hashset.Add(number);
+                }
+
+            var en = hashset.OrderBy(x => x);
+
+            foreach(var el in en)
+                Console.Write(el + " ");
+        }
+    }
+}
